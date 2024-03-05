fdffcecdc4b440fa7308450d2a23d5c7bf39aaf6
lpitto86 <luca.p481@gmail.com>
2024-03-05 18:47:37 +0100

    Comics

---
+++ app/Http/Controllers/Admin/comicController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use App\Models\Comic;
+
+use App\Http\Requests\Request\UpdateComicRequest;
+use App\Http\Requests\Request\StoreComicRequest;
+
+class ComicController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+            public function index()
+        {
+            $comics = Comic::all();
+        return view('comics.index', compact('comics'));
+        }
+    /**
+     * Show the form for creating a new resource.
+     */
+    public function create()
+    {
+        return view('comics.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        $comic = Comic::create($request->all());
+
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'thumb' => 'required|string',
+            'price' => 'required|numeric',
+            'series' => 'required|string',
+            'sale_date' => 'required|date',
+            'type' => 'required|string',
+            'artists' => 'required|string',
+            'writers' => 'required|string',
+        ]);
+
+        $comic = Comic::create($request->all());
+
+        return redirect()->route('comics.show', ['comic' => $comic->id]);
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(Comic $comic)
+    {
+        return view('comics.show', compact('comic'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     */
+    public function edit(Comic $comic)
+    {
+        return view('comics.edit', compact('comic'));
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Comic $comic)
+    {
+        $request->validate([
+            'title' => 'required|string|max:255',
+            'description' => 'required|string',
+            'thumb' => 'required|string',
+            'price' => 'required|numeric',
+            'series' => 'required|string',
+            'sale_date' => 'required|date',
+            'type' => 'required|string',
+            'artists' => 'required|string',
+            'writers' => 'required|string',
+        ]);
+
+        $comic->update($request->all());
+
+        return redirect()->route('comics.index', ['comic' => $comic->id]);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
--- app/Models/comic.php
+++ app/Models/comic.php
@@ -5,7 +5,10 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
-class Book extends Model
+
+class Comic extends Model
 {
-    use HasFactory;
+    protected $guarded = [
+        '_token'
+    ];
 }
